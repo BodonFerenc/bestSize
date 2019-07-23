f3fbeae11010c5ebf19f5180547f300494502cd9
BodonFerenc <bodonferenc@gmail.com>
2019-07-23 12:43:04 +0200

    adding python version of createMultiColTable

--- python/bestSizeFn.py
+++ python/bestSizeFn.py
@@ -1,18 +1,32 @@
 import numpy as np
 import pandas as pd
 
+PRICEDOMSIZE=  5  # domain size of prices
+SIZEDOMSIZE= 100
+
 def createTable(N):
     return pd.DataFrame({
-   'pA': np.random.randint(0, 5, N),
-   'pB': np.random.randint(0, 5, N),
-   'sA': np.random.randint(0, 100, N),
-   'sB': np.random.randint(0, 100, N)})
+   'pA': np.random.randint(0, PRICEDOMSIZE, N),
+   'pB': np.random.randint(0, PRICEDOMSIZE, N),
+   'sA': np.random.randint(0, SIZEDOMSIZE, N),
+   'sB': np.random.randint(0, SIZEDOMSIZE, N)})
+
+def createMultiColTable(N, pCols, sCols):
+   M = len(pCols)
+   if(M != len(sCols)):
+        raise ValueError("length of price and size column names must be equal")
+   return pd.DataFrame(
+        np.concatenate([np.random.randint(0, PRICEDOMSIZE, size=(N, M)), 
+                        np.random.randint(0, SIZEDOMSIZE,  size=(N, M))], axis=1), 
+        columns = pCols + sCols)
 
 def createArrayTable(N, M):
-    rowLength= 1 + np.random.randint(0, M, N)
+    rowLength = 1 + np.random.randint(0, M, N) 
     return pd.DataFrame({
-        'price': np.array([np.random.randint(0, 5, i) for i in rowLength]),
-        'size': np.array([np.random.randint(0, 100, i) for i in rowLength])})
+        'price': np.array([np.random.randint(0, 5, i) 
+                for i in rowLength]),
+        'size':  np.array([np.random.randint(0, 100, i) 
+                for i in rowLength])})
 
 def bestSizeIF(pA, pB, sA, sB):
     if(pA == pB):
